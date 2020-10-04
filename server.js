f386b17c7f5516126afd639f7d52d91265dc5762
Magdalena Świderska <m.swiderska@outlook.com>
2020-10-04 10:53:22 +0200

    Add db.js

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const app = express();
 const uuidv4 = require('uuidv4')
+const db = require('./db');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const db = [
-    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-    { id: 3, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 4, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-  ];
+
 
 app.get('/testimonials', (req, res) => {
     res.json(db.testimonials);
