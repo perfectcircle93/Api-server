f44780946ae5e54bba02dc57fc79566cca863f9e
Magdalena Świderska <m.swiderska@outlook.com>
2020-10-21 09:22:29 +0200

    Add yarn build for one port 8000

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -10,14 +11,22 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
   });
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+  app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+  });
+
