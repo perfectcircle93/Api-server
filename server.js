319cb5371787132bb74cf0ee67fda3dc90bec02e
Magdalena Świderska <m.swiderska@outlook.com>
2020-10-24 09:56:25 +0200

    Add socket.io

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const socket = require('socket.io');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -26,7 +27,13 @@ app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
   });
 
-  app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
   });
+
+const io = socket(server);
+
+io.on('connection', () => {
+console.log('New socket!')
+});
 
